1ed73667605427734f150d54ec876a9aab3a47ef
wagnerDuarteDTI <144842525+wagnerDuarteDTI@users.noreply.github.com>
2025-04-02 14:24:59 -0300

    project created

---
+++ swiftUI+API/swiftUI+API/swiftUI_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_APIApp.swift
+//  swiftUI+API
+//
+//  Created by Wagner Duarte on 02/04/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftUI+API/swiftUI+APITests/swiftUI_APITests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_APITests.swift
+//  swiftUI+APITests
+//
+//  Created by Wagner Duarte on 02/04/25.
+//
+
+import Testing
+@testable import swiftUI_API
+
+struct swiftUI_APITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
